3e99e146ac3f8bf03912adc903ae314e3d595e30
tathagata2005 <156827167+tathagata2005@users.noreply.github.com>
2024-09-17 21:16:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ let resetButton = document.querySelector("#reset-button");
 let newButton = document.querySelector("#new-button");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
-let turnO = true; // playerO starts
+let chooseOButton = document.querySelector("#choose-o");
+let chooseXButton = document.querySelector("#choose-x");
+let chooseContainer = document.querySelector(".choose-symbol");
+
+let playerSymbol = ""; 
+let computerSymbol = ""; 
+let turnO = true; 
 
 const winPatterns = [
     [0, 1, 2],
@@ -15,27 +21,40 @@ const winPatterns = [
     [3, 4, 5],
     [6, 7, 8]
 ];
+
 const resetGame = () => {
     turnO = true; 
     enableBoxes(); 
-    msgContainer.classList.add("hide"); 
+    msgContainer.classList.add("hide");
+    chooseContainer.classList.remove("hide");
+    playerSymbol = ""; 
+    computerSymbol = "";
 };
+
 const disableBoxes = () => {
     for (let box of boxes) {
         box.disabled = true;
     }
 };
+
 const enableBoxes = () => {
     for (let box of boxes) {
         box.disabled = false; 
         box.innerText = ""; 
     }
 };
+
 const showWinner = (winner) => {
     msg.innerText = `Congratulations, Winner is ${winner}`; 
     msgContainer.classList.remove("hide"); 
     disableBoxes(); 
 };
+
+const showDraw = () => {
+    msg.innerText = `It's a draw!`; 
+    msgContainer.classList.remove("hide"); 
+};
+
 const checkWinner = () => {
     for (let pattern of winPatterns) {
         let pos1Val = boxes[pattern[0]].innerText;
@@ -45,26 +64,58 @@ const checkWinner = () => {
             if (pos1Val === pos2Val && pos2Val === pos3Val) {
                 console.log("Winner", pos1Val);
                 showWinner(pos1Val); 
-                return;
+                return true; 
             }
         }
     }
+    return false; 
 };
+
+const checkDraw = () => {
+    
+    for (let box of boxes) {
+        if (box.innerText === "") {
+            return false; 
+        }
+    }
+    return true; 
+};
+
+const handleMove = (box) => {
+    console.log("The box was clicked");
+    if (turnO) {
+        box.innerText = playerSymbol; // Player's move
+        turnO = false;
+    } else {
+        box.innerText = computerSymbol; // Opponent's move
+        turnO = true;
+    }
+    box.disabled = true; 
+
+   
+    if (!checkWinner()) {
+        if (checkDraw()) {
+            console.log("It's a draw");
+            showDraw();
+        }
+    }
+};
+const chooseSymbol = (symbol) => {
+    playerSymbol = symbol;
+    computerSymbol = symbol === "O" ? "X" : "O";
+    chooseContainer.classList.add("hide"); 
+};
+
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
-        console.log("The box was clicked");
-        if (turnO) {
-            // Player O's turn
-            box.innerText = "O";
-            turnO = false;
-        } else {
-            // Player X's turn
-            box.innerText = "X";
-            turnO = true;
+        if (playerSymbol !== "" && computerSymbol !== "") { 
+            handleMove(box);
         }
-        box.disabled = true; 
-        checkWinner(); 
     });
 });
+
+chooseOButton.addEventListener("click", () => chooseSymbol("O"));
+chooseXButton.addEventListener("click", () => chooseSymbol("X"));
+
 newButton.addEventListener("click", resetGame); 
-resetButton.addEventListener("click", resetGame); 
+resetButton.addEventListener("click", resetGame);
